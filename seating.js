4f1db87336f08a89fd97507a72b72fd8f4e985f3
Caelan <caelan.borowiec@pqi.org>
2019-04-16 17:05:54 -0400

    Added row details, and findLargestGroup class function

--- seating.js
+++ seating.js
@@ -14,6 +14,7 @@ class seatingChart {
       throw "This seating arrangement has no seats.";
 
     this.reservedTo = "";
+    this.rowDetails = new Array(this.rows);
 
     // Create a seat grid
     var seatGrid = [];
@@ -21,9 +22,14 @@ class seatingChart {
     var emptyRow = new Array(this.rowLength);
     emptyRow.fill(0);
     // Set up a 2D array
-    for (var i = 0; i < this.rows; i++)
+    for (let i = 0; i < this.rows; i++)
     {
       seatGrid[i] = emptyRow.slice();
+      this.rowDetails[i] = {
+        freeSeats: this.rowLength,
+        largestGroup: this.rowLength,
+        firstFree: 0
+      }
     }
     this.allSeats = seatGrid;
   }
@@ -32,17 +38,38 @@ class seatingChart {
   {
     console.log(this.allSeats);
     console.log("Free seats: " + this.freeSeats);
+    console.log(this.rowDetails);
   }
 
   reserveSeat(row, column)
   {
-    if (this.allSeats[row-1][column-1] === 1) //add check if invalid
+    if (this.allSeats[row-1][column-1 != undefined && this.allSeats[row-1][column-1] === 1)
       return false;
     else {
       this.allSeats[row-1][column-1] = 1;
       this.freeSeats--;
+      this.rowDetails[row-1].freeSeats--;
+      this.rowDetails[row-1].largestGroup = this.findLargestGroup(row);
       return true;
     }
+  }
+  findLargestGroup(row)
+  {
+    var count = 0;
+    var largest = 0;
+    for (let i = 0; i < this.rowLength; i++)
+    {
+      if (this.allSeats[row-1][i] === true)
+        count++; // Seat is free, count up
+      else {
+        //seat is not free.
+        if (count > largest)
+        largest = count;
+        //reset count, and keep checking
+        count = 0;
+      }
+    }
+    return largest; //todo also return the start location of this block
   }
   firstFreeInRow(row)
   {
