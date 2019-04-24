0b8377a9679a9c9212ab1fb01d55a582b45a0cf4
Caelan <CaelanBorowiec@users.noreply.github.com>
2019-04-23 21:21:09 -0400

    Output changes
    Initial reservations are now marked as VIP (int:2)
    Error message added at the group reservation step, although this should never be needed due to a prior check when finding seat space.
    Added message saying "Not Available" if no seats can be found for a group.
    Added message to print the remaining number of seats at the end of the program.

--- driver.js
+++ driver.js
@@ -2,7 +2,7 @@ var puppetShow = new seatingChart(3, 11); //Create a seating group of 3 rows and
 var input = "R1C4 R1C6 R2C3 R2C7 R3C9 R3C10\n3\n3\n3\n1\n10"; // Todo replace placeholder with actual user input method
 var lines = input.split('\n');
 var regex = /[Rr](\d)[Cc](\d*)/;
-for(var i = 0;i < lines.length;i++)
+for(var i = 0; i < lines.length; i++)
 {
   if (i === 0)
   {
@@ -11,7 +11,7 @@ for(var i = 0;i < lines.length;i++)
     for (var group = 0; group < groups.length; group++)
     {
       var match = regex.exec(groups[group]);
-      if (puppetShow.reserveSeat(parseInt(match[1]), parseInt(match[2])))
+      if (puppetShow.reserveSeat(parseInt(match[1]), parseInt(match[2]), 1, 2))
         console.log("Reserved seat at: Row", match[1], "Column", match[2]);
     }
   }
@@ -22,7 +22,13 @@ for(var i = 0;i < lines.length;i++)
     let bestseats = puppetShow.findBestSeats(numSeats);
     if (bestseats != undefined && bestseats != -1)
     {
-      puppetShow.reserveSeat(bestseats[0], bestseats[1], numSeats);
+      let reserved = puppetShow.reserveSeat(bestseats[0], bestseats[1], numSeats);
+      if (!reserved)
+      {
+        console.log("An error occurred reserving", numSeats, "seats");
+        continue;
+      }
+
       let firstSeat = "R" + bestseats[0] + "C" + bestseats[1];
       if (numSeats == 1)
         console.log("Reserved seat", firstSeat);
@@ -32,5 +38,9 @@ for(var i = 0;i < lines.length;i++)
         console.log("Reserved seats: ", firstSeat, '-', lastSeat);
       }
     }
+    else
+      console.log("Not Available.");
   }
 }
+
+console.log("The remaining number of seats is:", puppetShow.freeSeats);
